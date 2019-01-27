fc037e2d2ab47599985fa5c62402dee3c79f0376
victorokech <vickesnet@gmail.com>
2019-01-27 23:43:39 +0000

    Merging changes with Kelvin and Beki v2

--- imports/ui/components/menu_preview.js
+++ imports/ui/components/menu_preview.js
@@ -1,46 +1,55 @@
-import { Meteor } from 'meteor/meteor';
-import { Template } from 'meteor/templating';
-import moment from 'moment';
-import { ReactiveVar } from 'meteor/reactive-var';
+import { Meteor } from "meteor/meteor";
+import { Template } from "meteor/templating";
+import moment from "moment";
+import { ReactiveVar } from "meteor/reactive-var";
 
-import './menu_preview.html';
-import { Menus } from '../../api/menus.js';
-import { Options } from "../../api/options"
-import { toUnicode } from 'punycode';
+import "./menu_preview.html";
+import { Menus } from "../../api/menus.js";
+import { Options } from "../../api/options";
+import { toUnicode } from "punycode";
 
-let weekStart = moment().startOf('isoWeek');
+let weekStart = moment().startOf("isoWeek");
 const weekMenu = new ReactiveVar([]);
 
 const createDays = () => {
     let newMenu = [];
-    const dayLabels = ['Sunday', 'Monday', 'Tuesday', 'Wednesdy', 'Thursday', 'Friday', 'Saturday'];
+    const dayLabels = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesdy",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    ];
 
     for (let i = 0; i < 5; i++) {
-        let menuday = weekStart.clone().add(i, 'days');
+        let menuday = weekStart.clone().add(i, "days");
         const label = dayLabels[menuday.day()];
-        newMenu.push(
-            {
-                date: menuday.format("YYYY-MM-DD"),
-                label
-            }
-        )
+        newMenu.push({
+            date: menuday.format("YYYY-MM-DD"),
+            label
+        });
     }
     weekMenu.set(newMenu);
-}
+};
 
 Template.Menu_preview.onCreated(function bodyOnCreated() {
-    Meteor.subscribe('menus');
-    Meteor.subscribe('options');
-})
+    Meteor.subscribe("menus");
+    Meteor.subscribe("options");
+});
 
 Template.Menu_preview.rendered = () => {
     const dateString = weekStart.format("YYYY-MM-DD");
     const menu = Menus.find({ startDate: dateString }).fetch()[0];
     if (typeof menu != "undefined") {
-        const options = Options.find({ menuId: menu._id, chosen: true }).fetch();
+        const options = Options.find({
+            menuId: menu._id,
+            chosen: true
+        }).fetch();
     }
     createDays();
-}
+};
 
 Template.Menu_preview.helpers({
     days() {
@@ -49,13 +58,19 @@ Template.Menu_preview.helpers({
     },
     breakfast(dateString) {
         // This helper populates the chosen breakfast meals
-        const breakfastOptions = Options.find({ date: dateString, mealtimeId: "Breakfast", chosen: true }).fetch();
+        const breakfastOptions = Options.find({
+            date: dateString,
+            mealtimeId: "Breakfast",
+            chosen: true
+        }).fetch();
         breakfastOptions.forEach((option, ind) => {
             //Get meals
             const meals = Options.getMeals(option._id);
             let mealString = "";
             meals.forEach((meal, ind) => {
-                if (ind) { mealString += " + "; }
+                if (ind) {
+                    mealString += " + ";
+                }
                 mealString += meal.name;
             });
             breakfastOptions[ind].meal = mealString;
@@ -64,13 +79,19 @@ Template.Menu_preview.helpers({
     },
     lunch(dateString) {
         // This helper populates the chosen lunch meals
-        const lunchOptions = Options.find({ date: dateString, mealtimeId: "Lunch", chosen: true }).fetch();
+        const lunchOptions = Options.find({
+            date: dateString,
+            mealtimeId: "Lunch",
+            chosen: true
+        }).fetch();
         lunchOptions.forEach((option, ind) => {
             //Get meals
             const meals = Options.getMeals(option._id);
             let mealString = "";
             meals.forEach((meal, ind) => {
-                if (ind) { mealString += " + "; }
+                if (ind) {
+                    mealString += " + ";
+                }
                 mealString += meal.name;
             });
             lunchOptions[ind].meal = mealString;
@@ -79,13 +100,19 @@ Template.Menu_preview.helpers({
     },
     supper(dateString) {
         // This helper populates the chosen lunch meals
-        const supperOptions = Options.find({ date: dateString, mealtimeId: "Supper", chosen: true }).fetch();
+        const supperOptions = Options.find({
+            date: dateString,
+            mealtimeId: "Supper",
+            chosen: true
+        }).fetch();
         supperOptions.forEach((option, ind) => {
             //Get meals
             const meals = Options.getMeals(option._id);
             let mealString = "";
             meals.forEach((meal, ind) => {
-                if (ind) { mealString += " + "; }
+                if (ind) {
+                    mealString += " + ";
+                }
                 mealString += meal.name;
             });
             supperOptions[ind].meal = mealString;
@@ -96,24 +123,25 @@ Template.Menu_preview.helpers({
         // This helper returns the date in human readable format
         const menus = Menus.find().fetch();
         menus.forEach((menu, ind) => {
-            const readableDate = moment(menu.startDate, "YYYY-MM-DD").format("MMM, Do YYYY");
-            (menus[ind]).readableDate = readableDate;
-            const thisWeek = moment().startOf('isoWeek').format("MMM, Do YYYY");
+            const readableDate = moment(menu.startDate, "YYYY-MM-DD").format(
+                "MMM, Do YYYY"
+            );
+            menus[ind].readableDate = readableDate;
+            const thisWeek = moment()
+                .startOf("isoWeek")
+                .format("MMM, Do YYYY");
             if (thisWeek == readableDate) {
-                (menus[ind]).default = true;
+                menus[ind].default = true;
             }
         });
         return menus;
     }
-})
+});
 
 Template.Menu_preview.events({
-    'change .date-select'(event) {
+    "change .date-select"(event) {
         const target = event.target;
         weekStart = moment(target.value, "MMM, Do YYYY");
         createDays();
     }
-})
-
-
-
+});
