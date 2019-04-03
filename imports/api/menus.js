12dd30f82515d1b03e7c13d7f92e9cbc38a24288
Bekteema <bmngulube@outlook.com>
2019-04-03 22:23:10 +0000

    Conflicts repaired.

--- imports/api/menus.js
+++ imports/api/menus.js
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-import { Meteor } from "meteor/meteor";
-import { Mongo } from "meteor/mongo";
-import { check } from "meteor/check";
-
-export const Menus = new Mongo.Collection('menus');
-// check if this executions are serverside
-if (Meteor.isServer) {
-
-  Meteor.publish('menus', function () {
-    return Menus.find({});
-  });
-}
-
-Meteor.methods({
-  //This method creates a menu item for a specific week. menuID - week and year.
-  'menu.create'(start, end) {
-    check(start, String);
-    check(end, String);
-
-    Menus.insert({
-      startDate: start,
-      endDate: end,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    });
-  }
-=======
 import { Meteor } from "meteor/meteor";
 import { Mongo } from "meteor/mongo";
 import { check } from "meteor/check";
@@ -53,5 +25,4 @@ Meteor.methods({
       updatedAt: new Date(),
     });
   }
->>>>>>> d579a2c4762c4c18f0540c9a1b7f537883bed8d9
 });
